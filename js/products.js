f0a15643a2c783b20905cc6d1007153c27d3a69e
Aswin Krishna <your_email@example.com>
2026-01-31 00:56:07 +0530

    Fix add to cart click handling and layout

--- js/products.js
+++ js/products.js
@@ -2,22 +2,39 @@ const storeData = localStorage.getItem("db")
 
 const display = document.getElementById("productList")
 
+
 let data = []
 
-if(storeData){
+if (storeData) {
 
-     data = JSON.parse(storeData)
- }
+    data = JSON.parse(storeData)
+}
 
- data.forEach(item=>{
+data.forEach(item => {
     const div = document.createElement("div")
-    div.className="productsCard"
-    div.innerHTML=`<img src="${item.url}" alt="" id="imageOfproduct">
-            <h3 id="nameOfproduct">${item.name}</h3>
-            <p id="priceOfproduct">₹${item.price}</p>
-            <button id="AddtoCart">Add to cart</button>`
-console.log(item.name,item.price)
-            display.append(div)
- })
+    div.className = "productsCard"
+    div.innerHTML = `<img src="${item.url}" alt="" class="imageOfproduct">
+            <h3 class="nameOfproduct">${item.name}</h3>
+            <p class="priceOfproduct">₹${item.price}</p>
+            <button class="AddtoCart">Add to cart</button>`
+    display.append(div)
 
- 
+    let cartItem =[]
+
+    const addTocart = div.querySelector(".AddtoCart")
+
+    display.addEventListener("click",function(event){
+if(event.target.classList.contains("AddtoCart")){
+    const cart = event.target.closest(".productsCard")
+    cartItem.push(cart)
+    console.log(cartItem)
+}
+        
+
+    }
+)
+
+
+})
+
+
